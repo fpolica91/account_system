57ae405c051866b2058c51b5c0df93e6ce4a2493
fabricio policarpo <fabriciopolicarpo0@gmail.com>
2021-02-14 15:14:38 -0500

    d

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 
-file = open("record.txt", "w+")
+file = open("record.txt", "w")
 
 class Account:
   account_number: None
@@ -8,6 +8,7 @@ class Account:
   last_name: None
   balance: None
   transactions: 0
+
   def read_data(self):
    self.account_number = input('what is your account number: ')
    self.first_name = input('what is your first name: ')
@@ -19,15 +20,19 @@ class Account:
    user_file.writelines(seq)
    user_file.close()
    print('Hey %s %s your account is all set!' % (self.first_name, self.last_name))
+
   def make_deposit(self):
     deposit = input('how much would you like to deposit? ')
     self.balance = (self.balance + int(deposit))
     self.transactions += 1
-    transactionHistory = [' transactions: ', str(self.transactions), ' balance: ', str(self.balance)]
+    transactionHistory =  "\n" + 'Your account balance is $' + str(self.balance) + "\n"
     file.writelines(transactionHistory)
     file.close()
+
   def get_balance(self):
-    return self.balance
+    file = open('record.txt', 'r')
+    print(file.read())
+
   def init_dummy(self):
     self.balance = 0
     self.transactions = 0
@@ -39,12 +44,11 @@ print('*** Acount Information System***')
 boolean = True
 while boolean: 
   print('Select an option from below ')
-  print('select 1 to make a deposit ')
-  print('Selet 2 to get your account balance ')
+  print('select 1 to make a deposit  \n')
+  print('Select 2 to get your account balance \n')
   selection = input(' make a selection: ')
   acct = Account()
   acct.init_dummy()
-
   if(int(selection) == 1):
     acct.read_data()
     acct.make_deposit()
